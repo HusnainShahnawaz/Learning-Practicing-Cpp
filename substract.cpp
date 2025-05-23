eae32f0470ad056e0c3803f70a1328c4fe703c24
Husnain Shahnawaz <husanainofficial2k5@gmail.com>
2025-05-23 16:22:49 +0500

    i have  learn this program substract

--- substract.cpp
+++ substract.cpp
@@ -13,18 +13,32 @@
 // }   
 
 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please neter the age of num2";
+//     cin>>num2;
+//     cout<<"subs is" <<num1-num2;
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
     int num1;
     int num2;
-    cout<<"please enter the age of num1";
+    cout<<"please neter the age of num1";
     cin>>num1;
-    cout<<"please neter the age of num2";
+    cout<<"please enter the age of num2";
     cin>>num2;
-    cout<<"subs is" <<num1-num2;
+    cout<<"subs is "<<num1-num2;
     return 0;
+
 }
-
-
