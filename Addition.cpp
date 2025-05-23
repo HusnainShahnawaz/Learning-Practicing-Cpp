52d63931b87b1cd48cb4bcc63ec497c4a0da59ed
mac <mac@macs-MacBook-Pro.local>
2025-05-23 15:45:58 +0500

    first commit

---
+++ Addition.cpp
@@ -0,0 +1,127 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1 \n";
+//     cin>>num1;
+//     cout<<"please enter the age of num2 \n";
+//     cin>>num2;
+
+//     cout<<"sum is " <<num1+num2<<endl;
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please neter the age of num2";
+//     cin>>num2;
+//     cout<<"sum is "<<num1+num2<<endl;
+//     return 0;
+// }
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please enter the age of num2";
+//     cin>>num2;
+//     cout<<"sum is "<<num1+num2;
+// }
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please neter the age of num1";
+//     cin>>num1;
+//     cout<<"please enter the age of num2";
+//     cin>>num2;
+//     cout<<"sum is"<<num1+num2;
+//     return 0;
+// }
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please neter the age of num1";
+//     cin>>num1;
+//     cout<< "please enter the age of num2";
+//     cin>>num2;
+//     cout<<"sum is "<<num1+num2;
+//     return 0;
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please eneter the age of num2";
+//     cin>>num2;
+//     cout<< "sum is" <<num1+num2;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please enter the age of num2 ";
+//     cin>>num2;
+//     cout<<"sum is "<<num1+num2;
+
+
+// }
+
+
+
+
+
+
+
+
+
---
+++ substract.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int num1;
+//     int num2;
+//     cout<<"please enter the age of num1";
+//     cin>>num1;
+//     cout<<"please enter the age of num2";
+//     cin>>num2;
+//     cout<<"subs is"<<num1-num2;
+//     return 0;
+// }   
+
+
+
